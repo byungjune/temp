b0b81defead33c8bfe2369386049f21065634add
Byungjune YOON <on.yoon7@gmail.com>
2019-01-14 00:41:57 +0900

    2019-01-14 00:41

---
+++ precache-manifest.6cc9c31610388b57ce5e8e58b865706a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5013f6cbc01832e2d2daacb3c962b10",
+    "url": "/static/media/no-pic.d5013f6c.jpg"
+  },
+  {
+    "revision": "89a54219fb1076668c89d43fec28abb9",
+    "url": "/static/media/logo.89a54219.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4e0b8f241b8fa399ab1e",
+    "url": "/static/js/main.4e0b8f24.chunk.js"
+  },
+  {
+    "revision": "5814c168d70eae786ef9",
+    "url": "/static/js/1.5814c168.chunk.js"
+  },
+  {
+    "revision": "4e0b8f241b8fa399ab1e",
+    "url": "/static/css/main.2009852a.chunk.css"
+  },
+  {
+    "revision": "5814c168d70eae786ef9",
+    "url": "/static/css/1.254b41fc.chunk.css"
+  },
+  {
+    "revision": "d0be73538dca8db9ebca58d0f8765b4d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c8f796efc2155347fda14dd8e9990b3.js"
+  "/precache-manifest.6cc9c31610388b57ce5e8e58b865706a.js"
 );
 
 workbox.clientsClaim();

fd5f038fda6cb9bf702cdad38a36be002e438173
Byungjune YOON <on.yoon7@gmail.com>
2019-01-21 23:34:17 +0900

    2019-01-21 23:31

---
+++ precache-manifest.d04d5b297bc0184b93ee34f20f48f3f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5013f6cbc01832e2d2daacb3c962b10",
+    "url": "/static/media/no-pic.d5013f6c.jpg"
+  },
+  {
+    "revision": "89a54219fb1076668c89d43fec28abb9",
+    "url": "/static/media/logo.89a54219.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a58c17bb29739acf3898",
+    "url": "/static/js/main.a58c17bb.chunk.js"
+  },
+  {
+    "revision": "19d2dfa8d825dd6f4aa0",
+    "url": "/static/js/1.19d2dfa8.chunk.js"
+  },
+  {
+    "revision": "a58c17bb29739acf3898",
+    "url": "/static/css/main.7bec977d.chunk.css"
+  },
+  {
+    "revision": "19d2dfa8d825dd6f4aa0",
+    "url": "/static/css/1.bd68e618.chunk.css"
+  },
+  {
+    "revision": "567e7daf9609a2981bcb5b20bfd7b64c",
+    "url": "/index.html"
+  }
+];
